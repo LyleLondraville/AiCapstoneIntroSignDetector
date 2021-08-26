41c1f9724e0f83910de73186d5ceb7bf4e638c5e
lylelondraville <lyle.londraville@gmail.com>
2021-08-26 16:31:02 -0400

    added response formating

--- ToneDetector.py
+++ ToneDetector.py
@@ -3,7 +3,10 @@ from ibm_watson import ToneAnalyzerV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 
 keys_file = open('Keys.json')
+responses_file = open('Responses.json')
+
 keys = json.load(keys_file)
+responses = json.load(responses_file)
 
 authenticator = IAMAuthenticator(keys['api-key'])
 tone_analyzer = ToneAnalyzerV3(
@@ -19,6 +22,19 @@ tone_analysis = tone_analyzer.tone(
     content_type="application/json"
 ).get_result()
 
-print(json.dumps(tone_analysis, indent=2))
+tone_confidence = 0
+most_likely_tone = None
+
+for tone in tone_analysis["document_tone"]["tones"]:
+    if tone_confidence < tone["score"]:
+        most_likely_tone = tone
+        tone_confidence = tone["score"]
+
+
+print("Hmmmmmmmmm")
+print("Im like")
+print("{score}% sure that like".format(score=(float(most_likely_tone["score"])*100.0)))
+print("ur being an {name}".format(name= responses[most_likely_tone["tone_id"]]))
 
 keys_file.close()
+responses_file.close()
